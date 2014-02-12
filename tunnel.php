0aa4a50dc1bfa61bc089420332a122c1162ecda8
Peteris Krumins <peteris.krumins@gmail.com>
2014-02-12 04:10:49 +0200

    added tunnel.php

---
+++ tunnel.php
@@ -0,0 +1,20 @@
+<?php
+ 
+// port that was returned by browserling tunnel
+$browserlingTunnelPort = 59374;
+
+// path to your wordpress installation
+// use "/" if there is no special path
+$browserlingWordpressPath = "/wp/";
+
+// that's it
+
+$browserlingTunnelUrl = "http://tunnel.browserling.com:${browserlingTunnelPortl}$browserlingWordpressPath";
+ 
+define('WP_SITEURL', $browserlingTunnelUrl);
+define('WP_HOME', $browserlingTunnelUrl);
+  
+update_option('siteurl', $browserlingTunnelUrl);
+update_option('home', $browserlingTunnelUrl);
+
+?>
